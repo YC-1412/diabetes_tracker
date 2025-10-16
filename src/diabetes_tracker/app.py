30fdbd65c9a3cb2b829cc42f6b40f95bc8057241
yc <yingyucao.yc@gmail.com>
2025-10-16 00:04:37 -0400

    lint

--- src/diabetes_tracker/app.py
+++ src/diabetes_tracker/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-from sqlalchemy.exc import SQLAlchemyError
 import os
 
 # Import our modules
